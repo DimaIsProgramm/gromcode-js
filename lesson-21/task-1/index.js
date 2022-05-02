1b91dd9d0d3ed32875aee14b0d0c05a329692f0d
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-05-02 13:10:35 +0300

    Add files via upload

---
+++ lesson-21/task-1/index.js
@@ -0,0 +1,11 @@
+export const getInputElement = titleElem => {
+  const myElem = document.querySelector('.title');
+  console.dir(myElem);
+  return myElem;
+};
+
+export const getInputElement = inputElem => {
+  const myInput = document.querySelector('input').querySelector('text');
+  console.dir(myInput);
+  return myInput;
+};
