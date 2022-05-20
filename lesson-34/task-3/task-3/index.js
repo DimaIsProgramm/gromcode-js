bbc8db2b6c8f0d7137dcb7e6633e87ae81923f0d
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-05-20 19:52:53 +0300

    Add files via upload

---
+++ lesson-34/task-3/task-3/index.js
@@ -0,0 +1,38 @@
+const formElem = document.querySelector('.login-form');
+const buttonSubmit = document.querySelector('.submit-button');
+
+const onInputChange = () => {
+  const isValidForm = formElem.reportValidity();
+  if (isValidForm) {
+    buttonSubmit.removeAttribute('disabled');
+  } else {
+    buttonSubmit.setAttribute('disabled', true);
+  }
+};
+
+formElem.addEventListener('input', onInputChange);
+
+const serverUrl = 'https://628622c1f0e8f0bb7c10e9d2.mockapi.io/api/v1/tasks';
+
+const createUserHandler = event => {
+  event.preventDefault();
+
+  const user = {
+    email: 'test@gmail.com',
+    name: 'Dima',
+    password: '123',
+  };
+  fetch(serverUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+    .then(response => response.json())
+    .then(body => {
+      alert(JSON.stringify(body));
+    });
+};
+
+formElem.addEventListener('submit', createUserHandler);
